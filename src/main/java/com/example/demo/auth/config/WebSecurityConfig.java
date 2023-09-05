272b394d22b361f55156139a074955b577345fa1
Nikolai <efremovnikolaus@gmail.com>
2023-09-06 00:56:24 +0200

    made little changes

--- src/main/java/com/example/demo/auth/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/auth/config/WebSecurityConfig.java
@@ -5,9 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collections;
 
 @Configuration
 
@@ -17,6 +24,7 @@ public class WebSecurityConfig {
         http.authorizeRequests().anyRequest().permitAll();
         return http.build();
     }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
---
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+                       id SERIAL PRIMARY KEY,
+                       password VARCHAR(100),
+                       username VARCHAR(50) UNIQUE
+);
+
+CREATE TABLE notes (
+                       id UUID PRIMARY KEY,
+                       access_type VARCHAR(255) NOT NULL,
+                       content VARCHAR(10000),
+                       name VARCHAR(100),
+                       author BIGINT,
+                       CONSTRAINT fk_author FOREIGN KEY (author) REFERENCES users(id)
+);
